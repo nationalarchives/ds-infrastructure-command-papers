a1e3506775029e6004b1b83b23a47bb1f1b95e48
karlkern <karl.kern@nationalarchives.gov.uk>
2024-01-15 08:37:05 +0000

    correct file location

--- components/terraform/cognito/main.tf
+++ components/terraform/cognito/main.tf
@@ -145,7 +145,7 @@ resource "aws_cognito_user_pool_ui_customization" "cp_cognito" {
     client_id = aws_cognito_user_pool_client.user_pool_client.id
 
     css        = ".label-customizable {font-weight: 400;}"
-    image_file = filebase64("logo-white.png")
+    image_file = filebase64("${path.module}/ui/logo-white.png")
 
     # Refer to the aws_cognito_user_pool_domain resource's
     # user_pool_id attribute to ensure it is in an 'Active' state
