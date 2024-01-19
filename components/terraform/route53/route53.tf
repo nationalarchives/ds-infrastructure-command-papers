fa9889fbda71cc272b5eb240b9cf6d43da0a2bf9
karlkern <karl.kern@nationalarchives.gov.uk>
2024-01-19 15:27:33 +0000

    remove ttl

--- components/terraform/route53/route53.tf
+++ components/terraform/route53/route53.tf
@@ -6,7 +6,6 @@ resource "aws_route53_record" "user_pool" {
     zone_id = aws_route53_zone.user_pool.zone_id
     name    = var.user_pool_domain
     type    = "A"
-    ttl     = 300
     alias {
         evaluate_target_health = false
 
