869b768f6c8f03807470821aa237b81808f2ad99
karlkern <karl.kern@nationalarchives.gov.uk>
2024-01-19 15:29:37 +0000

    change cert

--- components/terraform/data.tf
+++ components/terraform/data.tf
@@ -1,3 +1,3 @@
 data "aws_ssm_parameter" "cp_certificate_arn" {
-    name = "/infrastructure/certificate-manager/wildcard-certificate-arn"
+    name = "/infrastructure/certificate-manager/us-east-1-wildcard-certificate-arn"
 }
