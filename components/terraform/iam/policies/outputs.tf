ba063fc576983b388a63cad7e662779fcd8774df
Gnanesh V <msdn4gnanesh@gmail.com>
2024-02-07 14:44:29 +0000

    renamed the policy

--- components/terraform/iam/policies/outputs.tf
+++ components/terraform/iam/policies/outputs.tf
@@ -1,3 +1,3 @@
-output "commandpaper_policy_arn" {
-    value = aws_iam_policy.commandpaper_policy.arn
+output "command_papers_k8s_resource_access_policy_arn" {
+    value = aws_iam_policy.command_papers_k8s_resource_access_policy.arn
 }
