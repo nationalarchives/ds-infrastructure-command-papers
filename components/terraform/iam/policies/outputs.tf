43427c20daed6c2c69b30c0b756bc3646d6f28bb
karlkern <karl.kern@nationalarchives.gov.uk>
2024-02-22 11:33:45 +0000

    remove output

--- components/terraform/iam/policies/outputs.tf
+++ components/terraform/iam/policies/outputs.tf
@@ -1,6 +1,6 @@
 output "command_papers_k8s_resource_access_policy_arn" {
     value = aws_iam_policy.command_papers_k8s_resource_access_policy.arn
 }
-output "command_papers_github_ecr_assume_policy_arn" {
-    value = aws_iam_policy.command_papers_github_ecr_access_policy.arn
-}
+#output "command_papers_github_ecr_assume_policy_arn" {
+#    value = aws_iam_policy.command_papers_github_ecr_access_policy.arn
+#}
